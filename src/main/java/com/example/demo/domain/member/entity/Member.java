47d2efe03eca3b1f1a96d0bec5ab7ac0f2af6020
jhs512 <jangka512@gmail.com>
2023-09-14 13:44:18 +0900

    kakao 로그인

--- src/main/java/com/example/demo/domain/member/entity/Member.java
+++ src/main/java/com/example/demo/domain/member/entity/Member.java
@@ -26,6 +26,7 @@ public class Member {
     private String username;
     private String password;
     private String nickname;
+    private String profileImgUrl;
 
     public List<? extends GrantedAuthority> getGrantedAuthorities() {
         List<GrantedAuthority> grantedAuthorities = new ArrayList<>();
