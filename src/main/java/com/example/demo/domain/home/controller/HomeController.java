1e85baeeb4ab311b9d0425c3ed76590b8362b011
jhs512 <jangka512@gmail.com>
2023-09-14 13:16:09 +0900

    login form 정리

--- src/main/java/com/example/demo/domain/home/controller/HomeController.java
+++ src/main/java/com/example/demo/domain/home/controller/HomeController.java
@@ -1,19 +1,12 @@
 package com.example.demo.domain.home.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-
-import java.security.Principal;
 
 @Controller
 public class HomeController {
     @GetMapping("/")
-    public String showMain(Model model, Principal principal) {
-        String loginedMemberUsername = principal == null ? null : principal.getName();
-
-        model.addAttribute("loginedMemberUsername", loginedMemberUsername);
-
+    public String showMain() {
         return "usr/home/main";
     }
 }
